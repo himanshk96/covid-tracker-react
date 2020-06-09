33f92f4a60e6145b7d174daeca2d30aa73449583
Himanshu Kriplani <himanshukriplani@MacBook-Pro.home>
2020-06-08 22:47:48 -0700

    Updates

---
+++ precache-manifest.252730668020fcb7a25d5cfbd98c28ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c4b138317cffc35a420946d48aaad8",
+    "url": "/covid-tracker-react/index.html"
+  },
+  {
+    "revision": "908d1e85d87001dd98a2",
+    "url": "/covid-tracker-react/static/css/main.bb35f9f0.chunk.css"
+  },
+  {
+    "revision": "cd92bc022118af6ae57b",
+    "url": "/covid-tracker-react/static/js/2.649a17f2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker-react/static/js/2.649a17f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "908d1e85d87001dd98a2",
+    "url": "/covid-tracker-react/static/js/main.45b96c81.chunk.js"
+  },
+  {
+    "revision": "27231438ecd393eae8e2",
+    "url": "/covid-tracker-react/static/js/runtime-main.f1aa2698.js"
+  },
+  {
+    "revision": "e315501ccf100dc4a8e9ad78871dc8ee",
+    "url": "/covid-tracker-react/static/media/image.e315501c.jpg"
+  }
+]);
